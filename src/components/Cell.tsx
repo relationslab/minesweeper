3cff84c414157bb9b4670a1533be86c9089e252c
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2020-05-08 13:38:57 +0900

    add cell.tsx
    四角形の表示まで作成

---
+++ src/components/Cell.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledCell = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid #000;
+`;
+const Cell = () => {
+  return <StyledCell></StyledCell>;
+};
+
+export default Cell;
---
+++ src/containers/Cell.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Cell from "../components/Cell";
+
+const ContainerCell = () => {
+  return <Cell />;
+};
+export default ContainerCell;
