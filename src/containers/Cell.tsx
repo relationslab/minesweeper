58ebcdee353d0e4d0b88c6e5105addd28e963838
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2020-05-08 16:04:23 +0900

    add:flag
    右クリックで旗を立てる/消す機能を実装

--- src/containers/Cell.tsx
+++ src/containers/Cell.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Cell from "../components/Cell";
-import { openCell } from "../modules/Cell";
 import { RootState } from "../rootReducer";
+import { openCell, toggleFlag } from "../modules/Cell";
 
 const ContainerCell = () => {
   const dispatch = useDispatch();
   const cell = useSelector((state: RootState) => state.cell);
 
   const handleOpenCell = () => dispatch(openCell());
+  const handleToggleFlag = (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatch(toggleFlag());
+  };
 
-  const _props = { cell, handleOpenCell };
+  const _props = { cell, handleOpenCell, handleToggleFlag };
 
   return <Cell {..._props} />;
 };
