648f8e9f83cd6b8041a9e06c9bfad3558d5e9209
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2020-05-18 14:03:13 +0900

    add:SelectLevelコンポーネントの作成

--- src/reducers/Board/index.ts
+++ src/reducers/Board/index.ts
@@ -1,27 +1,64 @@
-import { ActionTypes, BoardState, BoardActionTypes } from "./types";
+import {
+  ActionTypes,
+  BoardState,
+  Level,
+  LevelKey,
+  BoardActionTypes,
+} from "./types";
 import { initializeBoard, openCell, toggleFlag } from "./helper";
 
+const level: Level = {
+  easy: {
+    width: 10,
+    height: 8,
+    mines: 10,
+  },
+  normal: {
+    width: 18,
+    height: 14,
+    mines: 40,
+  },
+  hard: {
+    width: 24,
+    height: 20,
+    mines: 99,
+  },
+};
+
+const { width, height, mines } = level.normal;
 const initialState: BoardState = {
-  cells: [],
-  width: 10,
-  height: 8,
+  cells: initializeBoard(width, height, mines),
+  width: width,
+  height: height,
+  mines: mines,
 };
 
 const reducer = (state = initialState, action: BoardActionTypes) => {
   switch (action.type) {
+    case ActionTypes.SELECT_LEVEL:
+      const { width, height, mines } = level[action.payload.level];
+      return {
+        cells: initializeBoard(width, height, mines),
+        width: width,
+        height: height,
+        mines: mines,
+      };
     case ActionTypes.CREATE_BOARD:
       return {
         ...state,
-        cells: initializeBoard(action.payload.width, action.payload.height),
+        cells: initializeBoard(
+          action.payload.width,
+          action.payload.height,
+          action.payload.mines
+        ),
       };
     case ActionTypes.OPEN_CELL:
-      const { width, height } = state;
       return {
         ...state,
         cells: openCell(
           state.cells,
-          width,
-          height,
+          state.width,
+          state.height,
           action.payload.x,
           action.payload.y
         ),
@@ -35,15 +72,27 @@ const reducer = (state = initialState, action: BoardActionTypes) => {
       return state;
   }
 };
+
+export const SelectLevelAction = (level: LevelKey): BoardActionTypes => {
+  return {
+    type: ActionTypes.SELECT_LEVEL,
+    payload: {
+      level: level,
+    },
+  };
+};
+
 export const createBoardAction = (
   width: number,
-  height: number
+  height: number,
+  mines: number
 ): BoardActionTypes => {
   return {
     type: ActionTypes.CREATE_BOARD,
     payload: {
       width: width,
       height: height,
+      mines: mines,
     },
   };
 };
