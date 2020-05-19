916699741d1492f307b96cc2be93753da6fb15aa
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2020-05-19 17:54:04 +0900

    add:ModalコンポーネントとRetryButtonコンポーネント

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import Header from "./components/Header";
 import Board from "./containers/Board";
 import { Provider } from "react-redux";
 import store from "./rootReducer";
+import Modal from "./containers/Modal";
 
 ReactDOM.render(
   <Provider store={store}>
     <Header />
     <Board />
+    <Modal />
   </Provider>,
   document.getElementById("root")
 );
